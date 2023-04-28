7476e67a160ad3785543ce1f927895bf59de6871
Mézes István <mezes.istvan@adb-pro.hu>
2023-04-28 15:28:42 +0200

    Szamok, Dolgozo feladatok

---
+++ 2023/04_28/Dolgozo/Dolgozo/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dolgozo
+{
+internal class Program
+{
+    class Dolgozo
+    {
+        public string Nev { get; set; }
+        public int Kor { get; set; }
+        public int Fizetes { get; set; }
+    }
+    
+    public static void Main(string[] args) {
+        List<Dolgozo> dolgozok = new List<Dolgozo>();
+        
+        for (int i = 1; i < 4; i++) {
+            Dolgozo dolgozo = new Dolgozo();
+            
+            Console.Write($"{i}. név: ");
+            dolgozo.Nev = Console.ReadLine();
+            Console.Write($"{i}. kor: ");
+            dolgozo.Kor = int.Parse(Console.ReadLine());
+            Console.Write($"{i}. fizetés: ");
+            dolgozo.Fizetes = int.Parse(Console.ReadLine());
+            
+            dolgozok.Add(dolgozo);
+        }
+        
+        Console.WriteLine("--------------------");
+        
+        Console.WriteLine($"Az átlagéletkor: {AtlagEletkor(dolgozok)}");
+        Dolgozo legnagyobbFizetesu = LegnagyobbFizetesuDolgozo(dolgozok);
+        Console.WriteLine($"A {legnagyobbFizetesu.Nev} nevű dolgozónak a legnagyobb a fizetése, ami {legnagyobbFizetesu.Fizetes} Ft.");
+        Dolgozo legfiatalabb = LegfiatalabbDolgozo(dolgozok);
+        Console.WriteLine($"A {legfiatalabb.Kor} éves {legfiatalabb.Nev} a legfiatalabb dolgozó, akinek a fizetése {legfiatalabb.Fizetes} Ft.");
+
+        Console.ReadKey();
+    }
+
+    static double AtlagEletkor(List<Dolgozo> lista) {
+        int osszeg = 0;
+        
+        foreach (Dolgozo dolgozo in lista) {
+            osszeg += dolgozo.Kor;
+        }
+        
+        return (double)osszeg / lista.Count;
+    }
+
+    static Dolgozo LegfiatalabbDolgozo(List<Dolgozo> lista) {
+        Dolgozo legfiatalabb = lista[0];
+        
+        foreach (Dolgozo dolgozo in lista) {
+            if (dolgozo.Kor < legfiatalabb.Kor) {
+                legfiatalabb = dolgozo;
+            }
+        }
+        
+        return legfiatalabb;
+    }
+
+    static Dolgozo LegnagyobbFizetesuDolgozo(List<Dolgozo> lista) {
+        Dolgozo legnagyobbFizetesu = lista[0];
+
+        foreach (Dolgozo dolgozo in lista) {
+            if (dolgozo.Fizetes > legnagyobbFizetesu.Fizetes) {
+                legnagyobbFizetesu = dolgozo;
+            }
+        }
+
+        return legnagyobbFizetesu;
+    }
+}
+}
---
+++ 2023/04_28/Szamok/Szamok/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Listak
+{
+internal class Program
+{
+    public static void Main(string[] args) {
+        List<int> szamok = new List<int>();
+
+        for (int i = 1; i < 6; i++) {
+            Console.Write($"{i}. szám: ");
+            int szam = int.Parse(Console.ReadLine());
+            szamok.Add(szam);
+        }
+
+        Console.WriteLine($"A számok összege: { Osszeg(szamok) }");
+        Console.WriteLine($"A számok átlaga: { Atlag(szamok)}") ;
+        Console.WriteLine($"A legnagyobb szám: { Maximum(szamok) }");
+        Console.WriteLine($"A legkisebb szám: { Minimum(szamok) }");
+        
+        Console.ReadKey();
+    }
+
+    static int Osszeg(List<int> lista) {
+        int osszeg = 0;
+        
+        foreach (var elem in lista) {
+            osszeg += elem;
+        }
+        
+        return osszeg;
+    }
+    
+    static double Atlag(List<int> lista) {
+        return (double)Osszeg(lista) / lista.Count;
+    }
+
+    static int Maximum(List<int> lista) {
+        int max = lista[0];
+
+        foreach (var elem in lista) {
+            if (max < elem) {
+                max = elem;
+            }
+        }
+
+        return max;
+    }
+
+    static int Minimum(List<int> lista) {
+        int min = lista[0];
+
+        foreach (var elem in lista) {
+            if (elem < min) {
+                min = elem;
+            }
+        }
+
+        return min;
+    }
+}
+}
